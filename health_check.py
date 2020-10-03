17c6858a066d3af5ac29cde4f508b6d023e58023
Yosef Cohen <yosefco12@gmail.com>
2020-10-03 09:05:25 +0300

    improve health_checks script

--- health_check.py
+++ health_check.py
@@ -20,21 +20,34 @@ def send_messege(title):
 
 def health_system():
     checks = {"cpu": None, "disk": None, "memory": None, "host": None}
-
+    messages = [
+        "Error - CPU usage is over 80%",
+        "Error - Available disk space is less than 20%",
+        "Error - Available memory is less than 500MB",
+        "Error - localhost cannot be resolved to 127.0.0.1",
+    ]
+    # checks if cpu load over 80%
     cpu_load = [x / os.cpu_count() * 100 for x in os.getloadavg()][-1]
-    total, used, free = shutil.disk_usage("/")
     if cpu_load > 80:
-        checks["cpu"] = "Error - CPU usage is over 80%"
+        checks["cpu"] = messages[0]
+        send_messege(checks["cpu"])
+
+    # checks if there are less then 20% disk space
+    total, used, free = shutil.disk_usage("/")
     if free / total < 0.2:
-        checks["disk"] = "Error - Available disk space is less than 20%"
+        checks["disk"] = messages[1]
         send_messege(checks["disk"])
+
+    # checks if available ram is more then 500M
     if psutil.virtual_memory()[1] < 500 * 1024 * 1024:
-        checks["memory"] = ["Error - Available memory is less than 500MB"]
+        checks["memory"] = messages[2]
         send_messege(checks["memory"])
+
+    # checks if local host points to 127.0.0.1
     with open("/etc/hosts", "r") as f:
         f = f.readline()
         if not ("localhost" in f.split() and "127.0.0.1" in f.split()):
-            checks["host"] = "Error - localhost cannot be resolved to 127.0.0.1"
+            checks["host"] = messages[3]
             send_messege(checks["hosts"])
 
 
